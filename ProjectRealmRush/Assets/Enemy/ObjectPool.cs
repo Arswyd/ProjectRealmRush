1f022301714a9495678d5afa99a0ff02c55e74f2
Arswyd <vdani94@gmail.com>
2021-03-18 22:27:09 +0100

    Added enemy instantiation

---
+++ ProjectRealmRush/Assets/Enemy/ObjectPool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject enemiePrefab;
+    [SerializeField] float spawnTimer = 1f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (Time.time < 5f)
+        {
+            Instantiate(enemiePrefab, transform);
+            yield return new WaitForSeconds(spawnTimer);
+        }
+
+    }
+}
