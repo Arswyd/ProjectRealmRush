9c06d00529147143ed808f0593e56bac323f8f66
Arswyd <vdani94@gmail.com>
2021-03-20 22:14:18 +0100

    Added castles and new tiles, created prototype level

---
+++ ProjectRealmRush/Assets/Environment/Waypoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] Tower towerPrefab;
+
+    [SerializeField] bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; } }
+
+    void OnMouseDown()
+    {
+        if(isPlaceable)
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
+            GetComponentInChildren<Light>().enabled = false;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (isPlaceable)
+        {
+            GetComponentInChildren<Light>().enabled = true;
+        }
+    }
+    void OnMouseExit()
+    {
+        if (isPlaceable)
+        {
+            GetComponentInChildren<Light>().enabled = false;
+        }
+    }
+}
